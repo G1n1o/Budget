9497ca7275f9cc32d095658585f5d53a8d8ddf12
G1n1o <rafal.szkudlarek88@gmail.com>
2024-04-09 05:48:24 +0200

    Update not-found.php

--- src/App/views/errors/not-found.php
+++ src/App/views/errors/not-found.php
@@ -1,16 +1,27 @@
 <?php include $this->resolve("partials/_header.php"); ?>
 
-<!-- Start Main Content Area -->
+
 
 <body>
-    <section class="container mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded text-center">
-        <h1 class="font-black text-9xl">404</h1>
-        <p class="my-6 font-bold text-xl">
-            The page you are looking for was moved, removed renamed or might never
-            existed.
-        </p>
-        <a href="/" class="bg-orange-400 text-white inline-block py-2 px-4 rounded">Go Home</a>
-    </section>
-    <!-- End Main Content Area -->
+
+    <?php include $this->resolve("partials/_mainNav.php"); ?>
+    <main>
+        <div class="hero">
+            <div class="hero-shadow"></div>
+            <div class="hero-main">
+
+                <div class="hero-main-text">
+                    <div class="home-page">
+                        <h1>
+                            Error 404
+                        </h1>
+                        <p>The page you are looking for was moved, removed renamed or might never
+                            existed.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </main>
 
     <?php include $this->resolve("partials/_footer.php"); ?>
